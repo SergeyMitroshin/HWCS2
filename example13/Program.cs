7e0f51cbd00a1cc365e005c7eb00139edc6e3f77
SergeyMitroshin <sergey@host-best.com>
2022-11-21 23:07:04 +0300

    v02

--- example13/Program.cs
+++ example13/Program.cs
@@ -8,4 +8,7 @@
 32679 -> 6
 */
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+if (s.Length < 3) Console.WriteLine("Третьей цифры нет"); else Console.WriteLine (s[2]);
+
